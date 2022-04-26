5035a2ac5f2f8f3812b53b64c5ebf4c1ed20f1a2
Wojciech Bojewski <wojciech.bojewski@gmail.com>
2022-04-27 00:34:51 +0200

    TButton moved to gui_engine

---
+++ gui_engine/__init__.py
@@ -0,0 +1,48 @@
+import pygame
+
+class GameParameters(object):
+    _instance = None
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(GameParameters, cls).__new__(cls)
+        return cls._instance
+
+class TButton():
+    def __init__(self):
+        self._enable = 0
+        self._x = 0
+        self._y = 0
+        self._label = ""
+        self._visible = 1
+    
+    def enable(self, e):
+        self._enable = e
+        return self
+
+    def position(self, x, y):
+        self._x = x
+        self._y = y
+        return self
+
+    def label(self, l):
+        self._label = l
+        return self
+
+    def visible(self, v):
+        self._visible = v
+        return self
+
+    def draw(self, screen):
+        font = pygame.font.SysFont('calibri.ttf', 50)
+        img = font.render(self._label, True, (255,255,255)) 
+
+        color = (255,0,0)
+        if self._enable==0:
+            color = (125,125,125)
+        pygame.draw.rect(screen, color, (self._x,self._y,23*len(self._label)+10,55))
+        screen.blit(img, (self._x+10, self._y+10))  
+
+    def isactive(self, x, y):
+        if (self._x) <= x <= (self._x)+23*len(self._label) and (self._y) <= y <= (self._y)+55:
+            return 1
+        return 0
--- pysudoku.py
+++ pysudoku.py
@@ -2,7 +2,7 @@
 import sudoku_engine as se
 import pygame
 import json
-
+from gui_engine import TButton
 
 class GameParameters(object):
     _instance = None
@@ -10,46 +10,6 @@ class GameParameters(object):
         if cls._instance is None:
             cls._instance = super(GameParameters, cls).__new__(cls)
         return cls._instance
-
-class TButton():
-    def __init__(self):
-        self._enable = 0
-        self._x = 0
-        self._y = 0
-        self._label = ""
-        self._visible = 1
-    
-    def enable(self, e):
-        self._enable = e
-        return self
-
-    def position(self, x, y):
-        self._x = x
-        self._y = y
-        return self
-
-    def label(self, l):
-        self._label = l
-        return self
-
-    def visible(self, v):
-        self._visible = v
-        return self
-
-    def draw(self, screen):
-        font = pygame.font.SysFont('calibri.ttf', 50)
-        img = font.render(self._label, True, (255,255,255)) 
-
-        color = (255,0,0)
-        if self._enable==0:
-            color = (125,125,125)
-        pygame.draw.rect(screen, color, (self._x,self._y,23*len(self._label)+10,55))
-        screen.blit(img, (self._x+10, self._y+10))  
-
-    def isactive(self, x, y):
-        if (self._x) <= x <= (self._x)+23*len(self._label) and (self._y) <= y <= (self._y)+55:
-            return 1
-        return 0
 
 with open(".\\gui_engine\\main_board.json") as file:
 		config = json.load(file)
@@ -62,8 +22,6 @@ for item in config["items"]:
                                    .position(config["items"][item]["xy"]["x"], config["items"][item]["xy"]["y"])\
                                    .visible(config["items"][item]["visible"])
                                    )
-
-
 
 pygame.init()
 pygame.display.set_caption(config["title"])
@@ -83,14 +41,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONUP:
-            #pos = event.pos
             for c in items:
                 if c._visible:
                     if c.isactive(event.pos[0],event.pos[1]):
                         print(f"Clicked on {c._label}")
 
     pygame.display.update()
-
 
 #fonts = pygame.font.get_fonts()
 #print(len(fonts))
